522a7442541923ca8f17956c3b337b3edee08a55
huynhngocnhuy234112e <yhnn234112e@st.uel.edu.vn>
2025-02-05 15:32:54 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+  base: "./", // Đảm bảo deploy đúng trên Vercel
+  build: {
+    outDir: "dist",
   },
-})
+});
